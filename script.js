6bf8f17b5ac2b46818a801a55b2139c0fd741d29
Gustavo Azzimonti Renzo <gazzimon@gmail.com>
2020-11-12 13:25:07 -0300

    prueba con GeoFindMe

---
+++ script.js
@@ -0,0 +1,27 @@
+ // ver el link: https://developer.mozilla.org/es/docs/WebAPI/Using_geolocation
+
+function geoFindMe() {
+    var output = document.getElementById("out");
+  
+    if (!navigator.geolocation){
+      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+      return;
+    }
+  
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+  
+   output.innerHTML = 'latitude: ' + latitude + '; longitude: ' + longitude + ';';
+    };
+  
+    function error() {
+      output.innerHTML = "Unable to retrieve your location";
+    };
+  
+    output.innerHTML = "<p>Locating…</p>";
+  
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+ 
